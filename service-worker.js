22d8a21dd58d17b15df903c7481957840f6e21a1
ABHI5425 <129541284+ABHI5425@users.noreply.github.com>
2025-05-10 00:04:24 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+// /service-worker.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  const { title, body } = data;
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body,
+      icon: '/icons/icon-192x192.png',
+      badge: '/icons/icon-96x96.png'
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
